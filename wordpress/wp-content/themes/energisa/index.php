42f4adb42a22e19e5a9300dd3b0596ba301e387a
Sérgio Machado <sergiojfjfjf@gmail.com>
2020-02-09 17:14:03 -0300

    Integrado o carrossel de produtos na página inicial

--- wordpress/wp-content/themes/energisa/index.php
+++ wordpress/wp-content/themes/energisa/index.php
@@ -299,89 +299,68 @@ if ($slider->have_posts()) :
             </div>
         </div>
     </section>
+
+
+<?php
+$produtos = new WP_Query(
+    array(
+        'post_type' => 'produtos',
+        'posts_per_page' => '6',
+    )
+);
+if ($produtos->have_posts()) :
+    $contador = 0;
+    $count = $produtos->found_posts;
+    ?>
     <section id="home-produtos">
         <div class="container-fluid p-0">
             <div id="carrouselProdutos" class="carousel slide" data-ride="carousel"> <!-- carousel-fade -->
+
                 <div class="carousel-side-indicators-holder">
                     <ol class="carousel-indicators">
-                        <li class="active mx-3" data-target="#carrouselProdutos" data-slide-to="0"></li>
-                        <li class="mx-3" data-target="#carrouselProdutos" data-slide-to="1"></li>
-                        <li class="mx-3" data-target="#carrouselProdutos" data-slide-to="2"></li>
+                        <?php
+                        for ($i = 0; $i < $count; $i++) { ?>
+                            <li class="<?php if ($i == 0) echo "active"; ?> mx-3" data-target="#carrouselProdutos" data-slide-to="<?php echo $i; ?>"></li>
+                        <?php }
+                        ?>
                     </ol>
                 </div>
-
                 <div class="carousel-inner ">
-                    <div class="carousel-item active carousel-long zoom-hover"
-                         style="
-                                 background: linear-gradient(0deg, rgba(8, 107, 192, 0.7), rgba(8, 107, 192, 0.7)), url(<?php bloginfo('template_url'); ?>/img/projeto-autoatendimento.png);
-                                 background-position: center;
-                                 background-size: cover;">
-                        <!-- <img src="https://via.placeholder.com/1442x854" class="d-block w-100" alt="..."> -->
-
-                        <div class="container custom-carousel-caption">
-                            <div class="slider-title slider-title-sm">
-                                <p class="text-uppercase">Alguns produtos</p>
-                                <h1>Totem Autoatendimento</h1>
-                                <p class="">É um fato conhecido de todos que um leitor se distrairá com o conteúdo de
-                                    texto legível de uma página quando estiver examinando sua diagramação</p>
-                                <button class="btn btn-primary px-5">Saiba mais</button>
-
+                    <?php
+                    while ($produtos->have_posts()): $produtos->the_post();
+                        $capa_produtos = get_the_post_thumbnail_url(null, 'full');
+                        $contador ++
+                        ?>
+                        <div class="carousel-item <?php if ($contador == 1) echo "active"; ?> carousel-long zoom-hover"
+                             style="background: linear-gradient(0deg, rgba(8, 107, 192, 0.7), rgba(8, 107, 192, 0.7)), url(<?php echo esc_url($capa_produtos); ?>);
+                                     background-position: center;
+                                     background-size: cover;">
+                            <div class="container custom-carousel-caption">
+                                <div class="slider-title slider-title-sm">
+                                    <p class="text-uppercase">Alguns produtos</p>
+                                    <h1><?php the_title(); ?></h1>
+                                    <p class=""><?php echo get_the_excerpt(); ?></p>
+                                    <a href="<?php the_permalink(); ?>" class="btn btn-primary px-5">Saiba mais</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="carousel-item  carousel-long zoom-hover"
-                         style="
-                                 background: linear-gradient(0deg, rgba(8, 107, 192, 0.7), rgba(8, 107, 192, 0.7)), url(<?php bloginfo('template_url'); ?>/img/projeto-autoatendimento.png);
-                                 background-position: center;
-                                 background-size: cover;">
-                        <!-- <img src="https://via.placeholder.com/1442x854" class="d-block w-100" alt="..."> -->
-
-                        <div class="container custom-carousel-caption">
-                            <div class="slider-title slider-title-sm">
-                                <p class="text-uppercase">Alguns produtos</p>
-                                <h1>Totem Autoatendimento</h1>
-                                <p class="">É um fato conhecido de todos que um leitor se distrairá com o conteúdo de
-                                    texto legível de uma página quando estiver examinando sua diagramação</p>
-                                <button class="btn btn-primary px-5">Saiba mais</button>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item  carousel-long zoom-hover"
-                         style="
-                                 background: linear-gradient(0deg, rgba(8, 107, 192, 0.7), rgba(8, 107, 192, 0.7)), url(<?php bloginfo('template_url'); ?>/img/projeto-autoatendimento.png);
-                                 background-position: center;
-                                 background-size: cover;">
-                        <!-- <img src="https://via.placeholder.com/1442x854" class="d-block w-100" alt="..."> -->
-
-                        <div class="container custom-carousel-caption">
-                            <div class="slider-title slider-title-sm">
-                                <p class="text-uppercase">Alguns produtos</p>
-                                <h1>Totem Autoatendimento</h1>
-                                <p class="">É um fato conhecido de todos que um leitor se distrairá com o conteúdo de
-                                    texto legível de uma página quando estiver examinando sua diagramação</p>
-                                <button class="btn btn-primary px-5">Saiba mais</button>
-
-                            </div>
-                        </div>
-                    </div>
-
+                    <?php endwhile;
+                    wp_reset_postdata(); ?>
                 </div>
                 <div class="container">
-
                     <div class="custom-control-carrousel-left custom-control-bottom-higher">
-
                         <a href="#carrouselProdutos" role="button" data-slide="prev"
                            class="btn btn-light btn-round"><span class="icon pt-2 pb-2 pl-1 icon-prev-icon"></span></a>
                         <a href="#carrouselProdutos" role="button" data-slide="next"
                            class="btn btn-light btn-round"><span class="icon pt-2 pb-2 pr-1 icon-next-icon"></span></a>
                     </div>
                 </div>
-
             </div>
         </div>
-
     </section>
+<?php endif; ?>
+
+
     <section class="pb-0" id="home-ideias">
         <div class="container mt-4 mb-0">
             <div id="parallax-detalhe-4">
