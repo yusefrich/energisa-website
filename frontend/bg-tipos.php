7ad57958e4a4e4d9b5bbfaca7f00c5752b7697c0
Yusef Richard <yusef.rick@gmail.com>
2020-02-18 11:59:44 -0300

    ajustes de bg cor

---
+++ frontend/bg-tipos.php
@@ -0,0 +1,296 @@
+<!doctype html>
+<html lang="en">
+
+<?php include "components/head.php"; ?>
+<?php include "components/header.php"; ?>
+<section class="bg-blue">
+    <div class="container-fluid p-0">
+
+        <div id="carrouselProdutosBlue" class="carousel slide" data-ride="carousel">
+            <!-- carousel-fade -->
+
+            <div class="carousel-inner ">
+                <div class="carousel-item active carousel-long zoom-hover">
+                    <!-- <img src="https://via.placeholder.com/1442x854" class="d-block w-100" alt="..."> -->
+
+                    <div class="container custom-carousel-caption">
+                        <div class="slider-title ">
+                            <div class="text-center mb-5 mx-5">
+                                <h2 class="display-h2 ">Olha o que vem por aí</h2>
+                                <p>Estamos cada vez mais empolgados para melhorar nossos serviços, acompanhe em que
+                                    estamos trabalhando atualmente</p>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div style="
+                                height: 431px;
+                                background-image: url(./img/product-info-full.png);
+                                background-size: cover;
+                                background-position: center;
+                                    " class="product-description-info">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <p class="text-uppercase">Alguns produtos</p>
+                                    <h2 style="width: 300px;" class="display-h2 ">Pague suas contas</h2>
+                                    <p class="">Lorem Ipsum é simplesmente uma simulação de texto da indústria
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando
+                                        um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para
+                                        fazer um livro de modelos de tipos.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+
+                <div class="custom-control-carrousel-center custom-control-bottom">
+
+                    <a href="#carrouselProdutosBlue" role="button" data-slide="prev"
+                        class="btn btn-light btn-round"><span class="icon pt-2 pb-2 pl-1 icon-prev-icon"></span></a>
+                    <a href="#carrouselProdutosBlue" role="button" data-slide="next"
+                        class="btn btn-light btn-round"><span class="icon pt-2 pb-2 pr-1 icon-next-icon"></span></a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</section>
+<section class="bg-white">
+    <div class="container-fluid p-0">
+
+        <div id="carrouselProdutosWhite" class="carousel slide" data-ride="carousel">
+            <!-- carousel-fade -->
+
+            <div class="carousel-inner ">
+                <div class="carousel-item active carousel-long zoom-hover">
+                    <!-- <img src="https://via.placeholder.com/1442x854" class="d-block w-100" alt="..."> -->
+
+                    <div class="container normal-carousel-caption">
+                        <div class="slider-title ">
+                            <div class="text-center mb-5 mx-5">
+                                <h2 class="display-h2 text-orange">Olha o que vem por aí</h2>
+                                <p>Estamos cada vez mais empolgados para melhorar nossos serviços, acompanhe em que
+                                    estamos trabalhando atualmente</p>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div style="
+                                height: 431px;
+                                background-image: url(./img/product-info-full.png);
+                                background-size: cover;
+                                background-position: center;
+                                    " class="product-description-info">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <p class="text-uppercase">Alguns produtos</p>
+                                    <h2 style="width: 300px;" class="display-h2 text-orange">Pague suas contas</h2>
+                                    <p class="">Lorem Ipsum é simplesmente uma simulação de texto da indústria
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando
+                                        um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para
+                                        fazer um livro de modelos de tipos.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+
+                <div class="custom-control-carrousel-center custom-control-bottom">
+
+                    <a href="#carrouselProdutosWhite" role="button" data-slide="prev"
+                        class="btn btn-primary btn-round"><span class="icon pt-2 pb-2 pl-1 icon-prev-icon"></span></a>
+                    <a href="#carrouselProdutosWhite" role="button" data-slide="next"
+                        class="btn btn-primary btn-round"><span class="icon pt-2 pb-2 pr-1 icon-next-icon"></span></a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</section>
+<section class="bg-gray">
+    <div class="container-fluid p-0">
+
+        <div id="carrouselProdutosGray" class="carousel slide" data-ride="carousel">
+            <!-- carousel-fade -->
+
+            <div class="carousel-inner ">
+                <div class="carousel-item active carousel-long zoom-hover">
+                    <!-- <img src="https://via.placeholder.com/1442x854" class="d-block w-100" alt="..."> -->
+
+                    <div class="container normal-carousel-caption">
+                        <div class="slider-title ">
+                            <div class="text-center mb-5 mx-5">
+                                <h2 class="display-h2 text-orange">Olha o que vem por aí</h2>
+                                <p>Estamos cada vez mais empolgados para melhorar nossos serviços, acompanhe em que
+                                    estamos trabalhando atualmente</p>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div style="
+                                height: 431px;
+                                background-image: url(./img/product-info-full.png);
+                                background-size: cover;
+                                background-position: center;
+                                    " class="product-description-info">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <p class="text-uppercase">Alguns produtos</p>
+                                    <h2 style="width: 300px;" class="display-h2 text-orange">Pague suas contas</h2>
+                                    <p class="">Lorem Ipsum é simplesmente uma simulação de texto da indústria
+                                        tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando
+                                        um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para
+                                        fazer um livro de modelos de tipos.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+
+                <div class="custom-control-carrousel-center custom-control-bottom">
+
+                    <a href="#carrouselProdutosGray" role="button" data-slide="prev"
+                        class="btn btn-primary btn-round"><span class="icon pt-2 pb-2 pl-1 icon-prev-icon"></span></a>
+                    <a href="#carrouselProdutosGray" role="button" data-slide="next"
+                        class="btn btn-primary btn-round"><span class="icon pt-2 pb-2 pr-1 icon-next-icon"></span></a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</section>
+<section class="bg-blue">
+    <div class="container-fluid p-0">
+
+        <div class="row  py-5 mr-2">
+            <div class="col-md-6">
+                <div style="
+                    height: 100%;
+                    background-image: url(./img/product-info-full.png);
+                    background-size: cover;
+                    background-position: center;
+                        " class="product-description-info">
+                </div>
+            </div>
+            <div class="col-md-6 text-white">
+                <div class="product-description-text pl-3">
+                    <h2 data-aos="fade-up" class="display-h2">
+                        Lorem ipsum dolor
+                    </h2>
+                    <p data-aos="fade-up">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e
+                        de impressos, e
+                        vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou</p>
+                    <h2 data-aos="fade-up" class="display-h2">
+                        Desafio
+                    </h2>
+                    <p data-aos="fade-up">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e
+                        de impressos, e
+                        vem sendo utilizado desde o século XVI,</p>
+                    <h2 data-aos="fade-up" class="display-h2">
+                        Produção
+                    </h2>
+                    <p data-aos="fade-up">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e
+                        de impressos, e
+                        vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja
+                        de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-white">
+    <div class="container-fluid p-0">
+
+        <div class="row  py-5 mr-2">
+            <div class="col-md-6">
+                <div style="
+                    height: 100%;
+                    background-image: url(./img/product-info-full.png);
+                    background-size: cover;
+                    background-position: center;
+                        " class="product-description-info">
+                </div>
+            </div>
+            <div class="col-md-6 ">
+                <div class="product-description-text pl-3">
+                    <h2 data-aos="fade-up" class="display-h2">
+                        Lorem ipsum dolor
+                    </h2>
+                    <p data-aos="fade-up">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e
+                        de impressos, e
+                        vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou</p>
+                    <h2 data-aos="fade-up" class="display-h2">
+                        Desafio
+                    </h2>
+                    <p data-aos="fade-up">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e
+                        de impressos, e
+                        vem sendo utilizado desde o século XVI,</p>
+                    <h2 data-aos="fade-up" class="display-h2">
+                        Produção
+                    </h2>
+                    <p data-aos="fade-up">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e
+                        de impressos, e
+                        vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja
+                        de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bg-gray">
+    <div class="container-fluid p-0">
+
+        <div class="row  py-5 mr-2">
+            <div class="col-md-6">
+                <div style="
+                    height: 100%;
+                    background-image: url(./img/product-info-full.png);
+                    background-size: cover;
+                    background-position: center;
+                        " class="product-description-info">
+                </div>
+            </div>
+            <div class="col-md-6 ">
+                <div class="product-description-text pl-3">
+                    <h2 data-aos="fade-up" class="display-h2">
+                        Lorem ipsum dolor
+                    </h2>
+                    <p data-aos="fade-up">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e
+                        de impressos, e
+                        vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou</p>
+                    <h2 data-aos="fade-up" class="display-h2">
+                        Desafio
+                    </h2>
+                    <p data-aos="fade-up">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e
+                        de impressos, e
+                        vem sendo utilizado desde o século XVI,</p>
+                    <h2 data-aos="fade-up" class="display-h2">
+                        Produção
+                    </h2>
+                    <p data-aos="fade-up">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e
+                        de impressos, e
+                        vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja
+                        de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include "components/footer.php"; ?>
+
+</body>
+
+</html>
