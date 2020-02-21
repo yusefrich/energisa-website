2f60c56bf1c33b896cebe3e0d768ccba108a7b5e
Yusef Richard <yusef.rick@gmail.com>
2020-02-21 09:30:31 -0300

    pagina titulo

---
+++ frontend/pagina-titulo.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<?php include "components/head.php"; ?>
+<?php include "components/header.php"; ?>
+<?php include "components/ideias/ideias-banner-small.php"; ?>
+<section class="mb-5 pb-5" id="pagina-titulo">
+    <div class="container">
+    <div class="d-flex justify-content-start my-4">
+            <a href="#carrouselProdutos" role="button" data-slide="prev" class="btn  btn-round p-0">
+            
+            <p class="text-breadcrumb font-weight-bold mt-1"><i class="fas pr-3 pt-1 fa-chevron-left"></i>Voltar</p>
+            </a>
+        </div>
+
+    <h2 class="font-weight-extra-bold text-caption display-h2">resultado da pesquisa</h2>
+
+    </div>
+</section>
+<?php include "components/footer.php"; ?>
+
+</body>
+
+</html>
