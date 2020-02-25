9f8f0904030d80f8b98fe16d3bad9972c82f4f9a
Yusef Richard <yusef.rick@gmail.com>
2020-02-25 18:55:27 -0300

    ajustes de formulario de ideias

--- wordpress/wp-content/themes/energisa/inc/painel.php
+++ wordpress/wp-content/themes/energisa/inc/painel.php
@@ -1,6 +1,6 @@
 <div class="col-md-12 d-flex justify-content-center align-items-center mb-5">
-    <a href="<?php the_permalink() ?>/painel" class="btn btn-info ml-3">Painel</a>
-    <a href="<?php the_permalink() ?>/submeter" class="btn btn-success ml-3">Enviar ideia</a>
-    <a href="<?php the_permalink() ?>/perfil" class="btn btn-primary ml-3">Editar meu perfil</a>
+    <a href="<?php the_permalink() ?>/painel" class="btn btn-light ml-3">Painel</a>
+    <a href="<?php the_permalink() ?>/perfil" class="btn btn-light ml-3">Editar meu perfil</a>
+    <a href="<?php the_permalink() ?>/submeter" class="btn btn-primary ml-3">Enviar ideia</a>
     <a href="<?php echo wp_logout_url(home_url()); ?>" class="btn btn-danger ml-3">Sair</a>
 </div>
