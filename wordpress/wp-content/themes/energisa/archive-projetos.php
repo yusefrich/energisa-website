7512a2b5c388d60edcaf454284baca0606f40f06
Yusef Richard <yusef.rick@gmail.com>
2020-02-26 11:48:02 -0300

    fixed projeto banner

--- wordpress/wp-content/themes/energisa/archive-projetos.php
+++ wordpress/wp-content/themes/energisa/archive-projetos.php
@@ -1,20 +1,18 @@
 <?php get_header(); ?>
 <section id="projetos-banner">
     <div style="
-            background: linear-gradient(0deg, rgba(92, 40, 14, 0.8), rgba(92, 40, 14, 0.8)), url(<?php bloginfo('template_url'); ?>/img/projeto-detail-header.png);
+            background: linear-gradient(0deg, rgba(92, 40, 14, 0.8), rgba(92, 40, 14, 0.8)), url(<?php bloginfo('template_url'); ?>/img/projetos-header.png);
             background-size: cover;
             background-position: center;
             background-blend-mode: multiply, normal;
             " class="container-fluid   product-banner-holder px-0 ">
-        <!-- text-white -->
-        <div class="product-banner text-center text-white ">
-            <p data-aos="fade-right" class="text-uppercase m-0">projeto</p>
-            <h1 data-aos="fade-right">Negociação de Dívidas</h1>
-            <p data-aos="fade-left" class="text-caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-                faucibus aenean amet
-                integer augue neque. At urna scelerisque blandit duis.</p>
+            <!-- text-white -->
+            <div class="product-banner text-center text-white ">
+                <h1 data-aos="fade-right">Veja nossos projetos</h1>
+                <p data-aos="fade-left" class="text-caption">Acompanhe todos os nossos projetos em andamento e fique por dentro das nossas atividades.</p>
+            </div>
         </div>
-    </div>
+
 </section>
 
 <section id="projetos-todos">
