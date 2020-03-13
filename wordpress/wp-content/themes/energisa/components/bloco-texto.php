8d4e5c7053ef38ef941364c3f94f461f2da6ce73
Yusef Richard <yusef.rick@gmail.com>
2020-03-13 15:25:39 -0300

    bloco de texto

---
+++ wordpress/wp-content/themes/energisa/components/bloco-texto.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+<section class="section"> <!-- adicionar classe bg-blue para bg azul, bg-white para branco, bg-gray para cinza -->
+    <div class="container ">
+        <div class="product-description-text  normal-detail-bg">
+
+            <!-- Verifica se tem algum valor cadastrado no campo repetidor-->
+            <h2 style="line-height: 50px;" data-aos="fade-up" class="display-h2 mb-4 aos-init aos-animate">
+                Título                                    
+            </h2>
+            <h3 style="line-height: 50px;" data-aos="fade-up" class=" mb-4 aos-init aos-animate">
+                Sub-titulo                                    
+            </h3>
+            <p class="mb-3 aos-init aos-animate" data-aos="fade-up">
+                É muito rápido, seguro e fácil. Com ele os clientes não perdem tempo e não gastam dinheiro imprimindo segunda via em outros lugares que cobram por isso.
+                É muito rápido, seguro e fácil. Com ele os clientes não perdem tempo e não gastam dinheiro imprimindo segunda via em outros lugares que cobram por isso.
+                É muito rápido, seguro e fácil. Com ele os clientes não perdem tempo e não gastam dinheiro imprimindo segunda via em outros lugares que cobram por isso.
+                É muito rápido, seguro e fácil. Com ele os clientes não perdem tempo e não gastam dinheiro imprimindo segunda via em outros lugares que cobram por isso.
+                É muito rápido, seguro e fácil. Com ele os clientes não perdem tempo e não gastam dinheiro imprimindo segunda via em outros lugares que cobram por isso.
+                É muito rápido, seguro e fácil. Com ele os clientes não perdem tempo e não gastam dinheiro imprimindo segunda via em outros lugares que cobram por isso.
+            </p>
+
+        </div>
+    </div>
+</section>
+
+<?php include "footer-nav.php"; ?>
+
+<?php get_footer(); ?>
